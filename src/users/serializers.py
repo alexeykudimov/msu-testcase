1bbdab109061e6ab5121b67492bdcef15b68fd2e
Alexey <work.alexeykudimov@gmail.com>
2021-11-18 17:46:33 +0300

    add password validator

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -11,7 +11,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
-    password = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
